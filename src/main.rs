05b7c0e598f77f6d258795d543a754eb5e0ddce0
Omar Zahid <omarzahid@outlook.com>
2025-05-13 02:17:40 +0800

    Day 3, Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,26 @@
 use std::fs;
 
-fn main() {
-    let input = fs::read_to_string("input-2").expect("No file.");
+use regex::Regex;
 
-    let mut reports = 0;
+fn main() {
+    let input = fs::read_to_string("input-3").expect("No file.");
+    let regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+
+    let mut result = 0;
 
     for line in input.lines() {
-        let levels: Vec<usize> = line
-            .split_whitespace()
-            .filter_map(|x| x.parse::<usize>().ok())
-            .collect();
+        let caps: usize = regex
+            .captures_iter(line)
+            .map(|caps| {
+                let (_, [a, b]) = caps.extract();
+                let left = a.parse::<usize>().unwrap_or(0);
+                let right = b.parse::<usize>().unwrap_or(0);
+                left * right
+            })
+            .sum();
 
-        if is_safe(&levels) {
-            reports += 1;
-        } else {
-            let mut safe_with_removal = false;
-            for i in 0..levels.len() {
-                let mut temp = levels.clone();
-                temp.remove(i);
-                if is_safe(&temp) {
-                    safe_with_removal = true;
-                    break;
-                }
-            }
-
-            if safe_with_removal {
-                reports += 1;
-            }
-        }
+        result = result + caps;
     }
 
-    println!("Number of safe reports::: {}", reports);
-}
-
-fn is_safe(levels: &[usize]) -> bool {
-    let mut direction: Option<&str> = None;
-
-    for window in levels.windows(2) {
-        let a = window[0];
-        let b = window[1];
-
-        let diff = a.abs_diff(b);
-
-        if diff > 3 || diff < 1 {
-            return false;
-        }
-
-        let current_dir = if b > a {
-            Some("inc")
-        } else if b < a {
-            Some("dec")
-        } else {
-            return false;
-        };
-
-        if let Some(dir) = direction {
-            if dir != current_dir.unwrap() {
-                return false;
-            }
-        } else {
-            direction = current_dir;
-        }
-    }
-    return true;
+    println!("Result for Day 3, part 1 is {}", result);
 }
