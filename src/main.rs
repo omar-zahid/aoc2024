fb5223d3847f25df4a3528f1d65a5333c33644d2
Omar Zahid <omarzahid@outlook.com>
2025-05-13 03:14:44 +0800

    Troubleshoot this

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ use regex::Regex;
 
 fn main() {
     let input = fs::read_to_string("input-3").expect("No file.");
-    let regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let input =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))".to_string();
+
+    let mul_match = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
 
     let mut result = 0;
 
-    for line in input.lines() {
-        let caps: usize = regex
+    let new_input = process_input(&input);
+    println!("{new_input}");
+
+    for line in new_input.lines() {
+        let caps: usize = mul_match
             .captures_iter(line)
             .map(|caps| {
                 let (_, [a, b]) = caps.extract();
@@ -22,5 +28,21 @@ fn main() {
         result = result + caps;
     }
 
-    println!("Result for Day 3, part 1 is {}", result);
+    println!("Result for Day 3, part 2 is {}", result);
+}
+
+fn process_input(input: &str) -> String {
+    let pat = Regex::new(r"(.*)don't\(\)(.*)do\(\)").unwrap();
+
+    let line: String = input.split_inclusive("don't()").collect();
+
+    let out: String = pat
+        .captures_iter(&line)
+        .map(|caps| {
+            let (_, [a, _b]) = caps.extract();
+            a
+        })
+        .collect();
+
+    out
 }
